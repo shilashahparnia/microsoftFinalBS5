a8471e641d79d7879e826a7c2edf6affb6bb801e
shila.shahparnia <shila.shahparnia@gmail.com>
2023-02-27 14:13:23 +0100

    deploying miscrosoft project

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper(".myShila", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+});
